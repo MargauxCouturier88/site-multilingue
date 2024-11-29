51720559ec28a4dce9646fcc3463c134a398adac
MargauxCouturier88 <margaux.couturier02@edu.univ-fcomte.fr>
2024-11-29 11:00:59 +0100

    dossier fr et en

---
+++ fr/src/js/script.js
@@ -0,0 +1,22 @@
+nav = document.querySelector("menu");
+
+nav.aria-hidden = false;
+
+toggle = ________.__________("________");
+
+toggle______________________________;
+
+const toggle = _____________________;
+const nav = ______________________;
+
+toggle.____________("______", () => {
+    nav__________________;
+    toggle__________________;
+  });
+
+
+
+const isOpen = toggle.ariaExpanded === "true";
+const isClosed = !isOpen;
+console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+```
